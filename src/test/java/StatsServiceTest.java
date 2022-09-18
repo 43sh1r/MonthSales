5f9291454023dc4eb1519b82039f8638b5d8a1f0
Gleb Trunov <gtrunov89@gmail.com>
2022-09-18 15:24:16 +0300

    1st

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,79 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.stats.StatsService;
+
+public class StatsServiceTest {
+
+    @Test
+    public void testSum() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testAvg() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testMinSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 9;
+        long actualMonth = service.minSales(sales);
+
+        Assertions.assertEquals(actualMonth, expectedMonth);
+    }
+
+    @Test
+    public void testMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 8;
+        long actualMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(actualMonth, expectedMonth);
+    }
+
+    @Test
+    public void testBelowAvg() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.lessThanAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testUpAvg() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.lessThanAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(actual, expected);
+    }
+}
+
